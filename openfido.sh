b25d47130dedfd779110c45fe355f63c8bc2cc80
johnsonhsiung <47260592+johnsonhsiung@users.noreply.github.com>
2022-02-28 13:03:09 -0800

    fixing missing default_billing

--- openfido.sh
+++ openfido.sh
@@ -23,6 +23,7 @@ gridlabd template config set GITREPO openfido-tariff-design
 gridlabd template config set GITBRANCH develop-tariff-design-openfido
 gridlabd template config set DATADIR /model/us/ca/slac
 gridlabd template get tariff_design
+cp default_billing.py /usr/local/opt/gridlabd/current/share
 
 echo "OPENFIDO_INPUT = $OPENFIDO_INPUT"
 echo "OPENFIDO_OUTPUT = $OPENFIDO_OUTPUT"
