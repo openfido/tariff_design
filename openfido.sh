17f2bf9e0845a4dd0cd9e6ae8c953a5aa5fe07ed
johnsonhsiung <47260592+johnsonhsiung@users.noreply.github.com>
2022-02-28 14:01:30 -0800

    transferring updates from old repo

--- openfido.sh
+++ openfido.sh
@@ -17,6 +17,14 @@ set -x
 # later, get the value and put it back
 # gridlabd -D suppress_repeat_messages=FALSE
 
+error()
+{
+    echo '*** ABNORMAL TERMINATION ***'
+    echo 'See error Console Output stderr for details.'
+    echo "See https://github.com/openfido/loadshape for help"
+    exit 1
+}
+
 # configure template 
 gridlabd template config set GITUSER johnsonhsiung
 gridlabd template config set GITREPO openfido-tariff-design
@@ -29,13 +37,13 @@ echo "OPENFIDO_INPUT = $OPENFIDO_INPUT"
 echo "OPENFIDO_OUTPUT = $OPENFIDO_OUTPUT"
 
 if ! ls -1 $OPENFIDO_INPUT/*.glm; then
-  echo "Input .glm file not found"
-  exit 1
+  echo "ERROR [openfido.sh]: input .glm file not found" > /dev/stderr
+  error
 fi
 
 if ! ls -1 $OPENFIDO_INPUT/config.csv; then
-  echo "Input config.csv file not found"
-  exit 1
+  echo "ERROR [openfido.sh]: input config.csv file not found" > /dev/stderr
+  error
 fi
 
 echo "Copying input files to working directory"
@@ -49,13 +57,16 @@ WEATHER_STATION=""
 WEATHER_STATION_INDEX_NUMBER=0
 
 python3 -m pip install -r  requirements.txt
-python3 csv_prepare.py 
 
-if [ $? != 0 ];
-then
-    exit 1 
+echo "Parsing config.csv..."
+python3 csv_prepare.py 
+echo "*** CONFIG.CSV PARSING SUCCESS ***"
+
+if [ $? != 0 ]; then
+    error
 fi
 
+echo "Matching weather station values..."
 # rows can be in any order
 while IFS=, read -r field1 field2 || [ -n "$field1" ]
 do
@@ -81,23 +92,31 @@ if [ $WEATHER_STATION_INDEX_NUMBER -eq 1 ] ; then
     WEATHER_STATION_PARSED=$(basename $WEATHER_STATION_LIST .tmy3)
     gawk -i inplace -F ',' '{gsub(find,replace,$2); print}' find="$WEATHER_STATION" replace="$WEATHER_STATION_PARSED" OFS="," config.csv
 elif [ $WEATHER_STATION_INDEX_NUMBER -gt 1 ] ; then
-    echo "ERROR [TARIFF_DESIGN] : Could not find unique weather station. Please specify from list below:\n$WEATHER_STATION_LIST"
-    exit 1
+    # printf for more consistent \n
+    printf "$WEATHER_STATION_LIST\n ERROR [TARIFF_DESIGN] : Could not find unique weather station. Please specify from list above:\n"  > /dev/stderr
+    error
 else 
-    echo "ERROR [TARIFF_DESIGN] : Could not find matching weather stations. Please check capitalization and spelling."
-    exit 1
+    echo "ERROR [TARIFF_DESIGN] : Could not find matching weather stations. Please check capitalization and spelling."  > /dev/stderr
+    error
 fi
-
+echo "*** WEATHER STATION UNIQUE MATCH SUCCESS ***"
 
 # put -t to get template online
-gridlabd $MODEL_NAME_INPUT -t tariff_design
-# gridlabd $MODEL_NAME_INPUT tariff_design.glm
+
+echo "Running gridlabd simulation..."
+gridlabd $MODEL_NAME_INPUT tariff_design.glm
 if [ $OUTPUT_NAME_INPUT != "output.csv" ]; then
   mv output.csv $OUTPUT_NAME_INPUT
 fi
 
 mv $OUTPUT_NAME_INPUT $OPENFIDO_OUTPUT
 
+echo '*** OUTPUTS ***'
+ls -l $OPENFIDO_OUTPUT
+
+echo '*** RUN COMPLETE ***'
+echo 'See Data Visualization and Artifacts for results.'
+
+echo '*** END ***'
 
 exit 0
-
