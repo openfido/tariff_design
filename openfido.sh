7814ec6639353bcfff1e0f9dc78c8a0d4618f3b0
johnsonhsiung <47260592+johnsonhsiung@users.noreply.github.com>
2022-05-23 17:38:58 -0700

    update github repo of template

--- openfido.sh
+++ openfido.sh
@@ -26,9 +26,9 @@ error()
 }
 
 # configure template 
-gridlabd template config set GITUSER johnsonhsiung
-gridlabd template config set GITREPO openfido-tariff-design
-gridlabd template config set GITBRANCH develop-tariff-design-openfido
+gridlabd template config set GITUSER slacgismo
+gridlabd template config set GITREPO gridlabd-template
+gridlabd template config set GITBRANCH add-tariff-design
 gridlabd template config set DATADIR /model/us/ca/slac
 gridlabd template get tariff_design
 
